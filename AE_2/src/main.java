f5423fe9556d0e2f4839f06d4a253db87ce3bf5a
Manuel <mpmalb@gmail.com>
2022-02-13 20:00:32 +0100

    Clase main modificada con pruebas de Resta

--- AE_2/src/main.java
+++ AE_2/src/main.java
@@ -16,8 +16,14 @@ public class main {
 		
 		// Probamos los métodos de la clase Resta.
 		
-		// Probamos los métodos de la clase Producto.
+		Resta resta1 = new Resta();
+		System.out.println(resta1.restaDosEnteros(85, 10));
+		System.out.println(resta1.restaDosReales(4.22f, 5.37f));
+		System.out.println(resta1.restaTresReales(5.37f, 3.78f, 5.99f));
+		resta1.restaAcumulada(6);
 		
+		// Probamos los métodos de la clase Producto.
+		        
 		// Probamos los métodos de la clase Cociente.	
 	}
 }
